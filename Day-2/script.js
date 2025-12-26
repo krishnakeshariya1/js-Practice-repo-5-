fef6bdfb95254600fb2c5ae60c09ff3e7caa59e6
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-12-26 13:27:13 +0530

    day 3 folder

--- Day-2/script.js
+++ Day-2/script.js
@@ -1,29 +1,32 @@
 // -------- Global Variables ---------- //
-let attemptCount = 3;
- let isCorrect = false;
+let attemptCount = 5;
+let isCorrect = false;
 // ---------- Random Value ----------- //
-let randomVal = Math.floor(Math.random()*10)+1;
+let randomVal = Math.floor(Math.random() * 50) + 1;
 
 while (attemptCount > 0) {
+
     let guessVal = Number(prompt("Guess a Number"));
 
-    if(Number.isNaN(guessVal) || guessVal > 10 || guessVal < 1){
-        alert("Enter a valide Number");
+    if (Number.isNaN(guessVal) || guessVal > 50 || guessVal < 1) {
+        console.error("invalide input")
+        continue;
     }
-    else if(guessVal === randomVal){
+    else if (guessVal === randomVal) {
         console.log("You have guess is Correct");
         isCorrect = true;
         break;
     }
-    else if(guessVal < randomVal){
+    else if (guessVal < randomVal) {
         console.log("guess is too low");
     }
-    else{
-        console.log("guess is too high");
+    else {
+        console.log("guess is too high"); 
     }
-    attemptCount --;
+    attemptCount--;
     console.log(`You have ${attemptCount} left`);
+
 }
-if(!isCorrect){
-    console.log("Game Over the Number was",randomVal)
+if (!isCorrect) {
+    console.log("Game Over the Number was", randomVal)
 }
