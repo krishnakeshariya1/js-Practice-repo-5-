162bd11a6fdf8879966f547c50ff5ec4f5f658bd
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-01-06 13:51:49 +0530

    done and delete functionality done

--- Day-10/script.js
+++ Day-10/script.js
@@ -3,45 +3,64 @@ const inputCategory = document.querySelector("#taskCategory");
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const taskContainer = document.querySelector(".taskContainer");
 
-let taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
+// Load tasks from localStorage or start with empty array
+let taskArray = (JSON.parse(localStorage.getItem("tasks")) || []);
 
+// Add task
 function handleInput() {
-    const taskName = inputName.ariaValueMax;
-    const taskCateg = inputCategory.value;
+    const taskName = inputName.value.trim();
+    const taskCateg = inputCategory.value.trim();
 
-    inputName.value = "";
-    inputCategory.value ="";
+    if (!taskName || !taskCateg) return; // ignore empty inputs
 
-    if (taskName === "" || taskCateg === "") return;
-
-    let task = {
-        Name : taskName,
+    const task = {
+        name: taskName,
         category: taskCateg,
-        isDone: false,
+        isDone: false
     };
 
     taskArray.push(task);
-    localStorage.setItem("tasks", task);
+    addToLocalstorage()
+
+    inputName.value = "";
+    inputCategory.value = "";
+
     renderCard();
 }
-
 function renderCard() {
     if (taskArray.length === 0) {
+        taskContainer.innerHTML = "<p class='para'>No tasks added</p>";
         return;
     }
-    taskContainer.innerHTML = "";
-    taskArray.forEach(task => {
-        taskContainer.innerHTML = `<div class="task">
-                <div class="rightContent">
-                    <h3>${task.Name}</h3>
-                    <h4>${task.category}</h4>
-                </div>
-                <div class="leftContent">
-                    <button>Done</button>
-                    <button>Delete</button>
-                </div>
-            </div>`
+
+    let html ="";
+    taskArray.forEach((task, index) => {
+        html +=`
+        <div class="task ${task.isDone ? "done" : "unDone"}">
+            <div class="rightContent">
+                <h3>${task.name}</h3>
+                <h4>${task.category}</h4>
+            </div>
+            <div class="leftContent">
+                <button onclick="markDone(${index})" class="">Done</button>
+                <button onclick="deleteTask(${index})">Delete</button>
+            </div>
+        </div>`;
+        taskContainer.innerHTML = html
     });
 }
+function deleteTask(indx) {
+    taskArray.splice(indx, 1);
+    addToLocalstorage()
+    renderCard();
+}
+function markDone(indx) {
+   taskArray[indx].isDone = true;
+   addToLocalstorage()
+   renderCard();
+}
 renderCard();
-addTaskBtn.addEventListener("click",handleInput)
+function addToLocalstorage() {
+ localStorage.setItem("tasks",JSON.stringify(taskArray))   
+}
+addTaskBtn.addEventListener("click",handleInput);
