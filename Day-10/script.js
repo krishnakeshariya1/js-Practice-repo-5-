4382b022363923bd57a5a79082e772332786a869
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-01-06 12:11:41 +0530

    render function add

--- Day-10/script.js
+++ Day-10/script.js
@@ -3,6 +3,45 @@ const inputCategory = document.querySelector("#taskCategory");
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const taskContainer = document.querySelector(".taskContainer");
 
-const taskArray = JSON.parse(localStorage.getItem("task")) || [];
+let taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
 
- 
+function handleInput() {
+    const taskName = inputName.ariaValueMax;
+    const taskCateg = inputCategory.value;
+
+    inputName.value = "";
+    inputCategory.value ="";
+
+    if (taskName === "" || taskCateg === "") return;
+
+    let task = {
+        Name : taskName,
+        category: taskCateg,
+        isDone: false,
+    };
+
+    taskArray.push(task);
+    localStorage.setItem("tasks", task);
+    renderCard();
+}
+
+function renderCard() {
+    if (taskArray.length === 0) {
+        return;
+    }
+    taskContainer.innerHTML = "";
+    taskArray.forEach(task => {
+        taskContainer.innerHTML = `<div class="task">
+                <div class="rightContent">
+                    <h3>${task.Name}</h3>
+                    <h4>${task.category}</h4>
+                </div>
+                <div class="leftContent">
+                    <button>Done</button>
+                    <button>Delete</button>
+                </div>
+            </div>`
+    });
+}
+renderCard();
+addTaskBtn.addEventListener("click",handleInput)
